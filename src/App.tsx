2d13b71afa349d8e2d8d510e121e883ad6393bf3
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2022-12-20 18:56:13 +0700

    add a dot

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      Готовимся к миддл-собеседованию
+      Готовимся к миддл-собеседованию.
     </div>
   );
 }
